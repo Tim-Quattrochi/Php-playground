1aab2708d0ebe32231b61320e0ed93ed36b0c14c
Tim Quattrochi <timq82@gmail.com>
2023-09-16 23:19:03 -0400

    set up Database class, simple query

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php'; //intentionally commented out to test Database class
+require 'Database.php';
+
+
+$config = require 'config.php';
+
+$db = new Database($config['database'], $config['database']['dbusername'], $config['database']['dbpass']);
+
+
+// :: is the scope resolution operator
+$posts = $db->query('SELECT * FROM posts')->fetchAll();
+
+dd($posts);
